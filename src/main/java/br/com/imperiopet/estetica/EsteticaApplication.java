a5f7f7ecf94b344a9d2e544029c6caab68d95c0a
paulo <pbrsj1385@gmail.com>
2022-12-16 09:22:47 -0300

    inicia projeto

---
+++ src/main/java/br/com/imperiopet/estetica/EsteticaApplication.java
@@ -0,0 +1,13 @@
+package br.com.imperiopet.estetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsteticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/imperiopet/estetica/EsteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imperiopet.estetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsteticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
