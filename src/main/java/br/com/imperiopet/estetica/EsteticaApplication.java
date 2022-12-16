58bf73ecc9470601994d31c3dacf481a2694ef0d
paulo <pbrsj1385@gmail.com>
2022-12-16 10:50:03 -0300

    comfigura applicatio yml e implementa api

--- src/main/java/br/com/imperiopet/estetica/EsteticaApplication.java
+++ src/main/java/br/com/imperiopet/estetica/EsteticaApplication.java
@@ -2,10 +2,15 @@ package br.com.imperiopet.estetica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class EsteticaApplication {
-
+	@GetMapping
 	public static void main(String[] args) {
 		SpringApplication.run(EsteticaApplication.class, args);
 	}
