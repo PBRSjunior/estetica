15ba842f4ba07d95cd7edf99349382c0883130dc
paulo <pbrsj1385@gmail.com>
2023-01-06 15:25:53 -0300

    implementa request e response

--- src/main/java/br/com/imperiopet/estetica/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/imperiopet/estetica/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,19 @@
 package br.com.imperiopet.estetica.cliente.application.api;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public interface ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	private String celular;
+	@NotBlank
+	private String endereco;
+	@CPF
+	private String cpf;
 
 }
